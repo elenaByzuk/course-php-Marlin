1b163229c9a7053b67c6cfa125d340dedf577b53
Elena Buziuk <elena.buzyk@gmail.com>
2022-11-14 15:47:26 +0200

    register files add

--- save_11.php
+++ save_11.php
@@ -1,6 +1,6 @@
 <?php
 $text = $_POST['text'];
-$pdo = new PDO("mysql:host=127.0.0.1;dbname=test", "root", "");
+$pdo = new PDO("pgsql:host=127.0.0.1;dbname=test", "postgres", "root");
 $sql = "INSERT INTO my_table (text) VALUES (:text)";
 $statement = $pdo->prepare($sql);
 $statement->execute(['text' => $text]);
